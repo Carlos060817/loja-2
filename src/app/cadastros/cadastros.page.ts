660845d5ac40a3a5a8f884efb0dc420069c0cdef
carlos060817 <cdaniel-011@hotmail.com>
2021-05-20 14:39:59 -0300

    Atualização 20.08

--- src/app/cadastros/cadastros.page.ts
+++ src/app/cadastros/cadastros.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService } from '../estoques/estoques.services';
 
 @Component({
   selector: 'app-cadastros',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrosPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private listEstoque : ListService) {
+   
   }
 
+ ngOnInit(): void {
+ }
+
+ onSubmit(form:any){
+ let id : number 
+ let cod : number 
+ let nome : string = ''
+ let entrada : number 
+ let saida : number 
+ let qtde : number 
+
+ id = form.value.id
+ cod = form.value.cod
+ nome = form.value.nome
+ entrada = form.value.entrada
+ saida = form.value.saida
+ qtde = form.value.qtde
+
+ this.listEstoque.addProduto(id,cod,nome,entrada,saida,qtde)
+
+ }
 }
